50b964296832f4e04cb394340d3955c23f6415ec
Moonvel <moonvel@yandex.ru>
2024-07-26 14:12:23 +0300

    specifications added

--- src/test/java/ru/moonvel/kuCoinApi/ApiTests.java
+++ src/test/java/ru/moonvel/kuCoinApi/ApiTests.java
@@ -3,21 +3,21 @@ package ru.moonvel.kuCoinApi;
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import io.restassured.http.ContentType;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import ru.moonvel.kuCoinApi.tools.HighToLowChangeRateComparator;
+import ru.moonvel.kuCoinApi.tools.ApiSpecification;
 
 public class ApiTests {
 
   public static final String BASE_URL = "https://api.kucoin.com/";
 
   public List<TickerData> getTickers() {
+    ApiSpecification.installSpecification(ApiSpecification.requestSpec(BASE_URL), ApiSpecification.responseSpecOK200());
     return given()
-        .contentType(ContentType.JSON)
         .when()
-        .get(BASE_URL + "api/v1/market/allTickers")
+        .get("api/v1/market/allTickers")
         .then()
         .extract().jsonPath().getList("data.ticker", TickerData.class);
   }
